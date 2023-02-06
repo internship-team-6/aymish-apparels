3c2d15f90cb292ce710dddadd6d0e9297572433e
Soumya B Hudalimath <unbxd@unbxds-MacBook-Pro.local>
2023-02-06 22:58:17 +0530

    fix: Fix the previous commit for frontend

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,89 +1,87 @@
+const searchQuery = function () {
+  const q = document.getElementById("searchid").value;
+  location.href = "./search-product-list.html?" + new URLSearchParams({ q: q });
+};
 
-
-function myQuery() {
-  localStorage.setItem("query", document.getElementById("searchid").value);
-  window.location.href = "http://localhost:5500/frontend/product-list.html?";
-}
+const categorySelect = function (catlevel1Id, catlevel2NameObj) {
+  const catlevel2Name = catlevel2NameObj.value;
+  location.href =
+    "./category-product-list.html?" +
+    new URLSearchParams({
+      catlevel1Id: catlevel1Id,
+      catlevel2Name: catlevel2Name,
+    });
+};
 
 const createNav = function () {
   let nav = document.querySelector("#navbar");
-
   nav.innerHTML = `
-    <nav id="navbar">
-      <div class="nav">
-        <a href="index.html"
-          ><img
-            src="img/final_logo.png"
-            class="brand-logo"
-            alt=""
-        /></a>
-
-        <div class="nav-items">
+    <div class="nav">
+      <a href="index.html"><img src="img/final_logo.png" class="brand-logo" alt="" /></a>
+      <div class="nav-items">
         <div class="search">
-        <form class = "form-bar">
-        <input
-        type="text"
-        class="search-box"
-        id="searchid"
-        placeholder="search brand, product"
-      />
-      <button class="search-btn" type="button" onclick="myQuery()">search</button>
-      </form>
-          </div>
+          <form class="form-bar">
+            <input type="text" class="search-box" id="searchid" placeholder="search brand, product" />
+            <button class="search-btn" type="button" onclick="searchQuery()">search</button>
+          </form>
         </div>
       </div>
-
-      
-
-      <div class="cat">
-  <select name="catm" id="catm" onchange="categorycall('men')">
-    <option value="" selected disabled hidden>MEN</option>
-    <option value="">All</option>
-    <option value="New Arrivals">New Arrivals</option>
-    <option value="Labels We Love">Labels We Love</option>
-    <option value="Tops">Tops</option>
-    <option value="Underwear & Lounge">Underwear & Lounge</option>
-    <option value="Suiting">Suiting</option>
-    <option value="Clearance Men">Clearance Men</option>
-    <option value="Express x NBA">Express x NBA</option>
-    <option value="Pants">Pants</option>
-    <option value="Accessories & Shoes">Accessories & Shoes</option>
-    <option value="Outerwear">Outerwear</option>
-    <option value="Accessories">Accessories</option> 
-  </select>
-
-  <select name="catw" id="catw" onchange="categorycall('men')">
-    <option value="" selected disabled hidden>WOMEN</option>
-    <option value="">All</option>
-    <option value="Clearance  Women">Clearance  Women</option>
-    <option value="Tops">Tops</option>
-    <option value="Jeans">Jeans</option>
-    <option value="Labels We Love">Labels We Love</option>
-    <option value="Jewelry">Jewelry</option>
-    <option value="Accessories">Accessories</option>
-    <option value="Dresses">Dresses</option>
-    <option value="Petite Clothing">Petite Clothing</option>
-    <option value="New Arrivals">New Arrivals</option>
-    <option value="Work">Work</option>
-    <option value="Jackets & Coats">Jackets & Coats</option>
-    <option value="Bottoms">Bottoms</option>
-    <option value="Sweaters & Cardigans">Sweaters & Cardigans</option>
-    <option value="Accessories & Shoes">Accessories & Shoes</option>
-    <option value="Labels We Love">Labels We Love</option>
-  </select>
-
-  <select name="cat1" id="cat1" onchange="categorycall('men')">
-    <option value="" selected disabled hidden>EXP</option>
-    <option value="">All</option>
-  </select>
-</div>
-
-      
-    </nav>
+    </div>
+    <div class="cat" id="cat-level-1">
+    </div>
     `;
+  fetch("http://localhost:5000/navbar", {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const categories_arr = data;
+      let cat_level_1 = document.getElementById("cat-level-1");
+      for (let i = 0; i < categories_arr.length; i++) {
+        let catlevel1Name = categories_arr[i]["name"];
+        let catlevel1Id = categories_arr[i]["id"];
+        cat_level_1.innerHTML += `
+          <select name="${catlevel1Name}" id="${catlevel1Id}" onchange="categorySelect(${catlevel1Id}, this)">
+            <option value="" selected disabled hidden>${catlevel1Name}</option>
+            <option value="">All</option>
+          </select>
+        `;
+        fetch(
+          "http://localhost:5000/dropdown?" +
+            new URLSearchParams({ catlevel1Id: catlevel1Id }),
+          {
+            method: "GET",
+            mode: "cors",
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            cat_level_1_id_element = document.getElementById(catlevel1Id);
+            for (let i = 0; i < data.length; i++) {
+              let val = data[i]["name"];
+              cat_level_1_id_element.innerHTML += `
+                <option id="${val}" value="${val}">
+                  ${val}
+                </option>
+              `;
+            }
+          });
+      }
+    });
 };
 
-createNav();
+onload = createNav();
 
 // function passProdDet(prodTitle, prodImg, prodPrice) {
 //   return `<div class="product-container" id="product-list-div">
--- frontend/js/product.js
+++ frontend/js/product.js
@@ -3,35 +3,32 @@ window.onload = function () {
   var prod_div = document.getElementById("product-details");
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product_id_search = urlParams.get("uid");
-  const product_id_cat = urlParams.get("catuid");
-  if (product_id_search != null) {
-    fetch(`http://127.0.0.1:5000/product?uid=${product_id_search}`, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        try {
-          console.log(data);
-          //   var filtered_data = data[1][0];
-          const products_arr = data;
-
-          //   var tempid = String(filtered_data.uniqueId);
-          var tempid = String(products_arr[0]);
-          let title = products_arr[1];
-          let img = products_arr[5];
-          let price = products_arr[4];
-          let desc = products_arr[3];
-          console.log(tempid);
-          prod_div.innerHTML += `<section id="product-details">
+  const uniqueId = urlParams.get("uniqueId");
+  if (uniqueId === null) {
+    return;
+  }
+  console.log(uniqueId)
+  fetch(`http://127.0.0.1:5000/product?uniqueId=${uniqueId}`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const productDict = data;
+      console.log(productDict);
+      // const uniqueId = String(productDict["id"]);
+      const title = productDict["title"];
+      const image = productDict["image"];
+      const price = productDict["price"];
+      const description = productDict["description"];
+      prod_div.innerHTML += `<section id="product-details">
           <div class="image-slider">
-          <img id="product_image" src="${img}" />
+          <img id="product_image" src="${image}" />
           </div>
           <div class="details">
             <p class="product-title">
@@ -39,15 +36,12 @@ window.onload = function () {
             </p>
             <span class="product-price">$${price}</span>
             <p class="product-des">
-              ${desc}
+              ${description}
             </p>
           </div>
         </section>`;
-        } catch (err) {
-          console.log(err);
-          // window.location = "404.html";
-        }
-        // document.getElementById("loader").style.display = "none";
-      });
-  }
+      // window.location = "404.html";
+      // document.getElementById("loader").style.display = "none";
+    })
+    .catch((err) => console.log(err));
 };

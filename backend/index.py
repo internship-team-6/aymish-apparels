85a412c3fda29b1845cf46e94a883330187ee126
mgpinf <manishgowd09@gmail.com>
2023-02-18 20:53:21 +0530

    feat: add full working version to docker

--- backend/index.py
+++ backend/index.py
@@ -34,5 +34,4 @@ api.add_resource(RecommendationsControl, "/recommendations")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
